bc341ee5d425b6e14b5a55f7b8bf732784c4758d
Patrick Sanfiel <patricksanfiel@gmail.com>
2019-02-22 14:49:08 -0500

    Updates

---
+++ precache-manifest.eda1d794497dad4cac427f949518e66f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cde52270e0c0c5f882e5e6e0b9117509",
+    "url": "/Pokedex/static/media/pokeball.cde52270.svg"
+  },
+  {
+    "revision": "b3468cd3de89372a17d9",
+    "url": "/Pokedex/static/js/runtime~main.b3468cd3.js"
+  },
+  {
+    "revision": "36ffb56fc43352bb34ca",
+    "url": "/Pokedex/static/js/main.36ffb56f.chunk.js"
+  },
+  {
+    "revision": "1015926caf23046f2ba3",
+    "url": "/Pokedex/static/js/1.1015926c.chunk.js"
+  },
+  {
+    "revision": "36ffb56fc43352bb34ca",
+    "url": "/Pokedex/static/css/main.63c7aa30.chunk.css"
+  },
+  {
+    "revision": "5c9b59f019626823c09083e38b6ae02a",
+    "url": "/Pokedex/index.html"
+  }
+];
